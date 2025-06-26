6206d37c87cb0cce741e9bc069f0c1eb256de3e5
Stefan Sullivan <stefan.sullivan@gmail.com>
2025-06-25 17:49:09 -0700

    add a config schema

--- js/config.js
+++ js/config.js
@@ -1,19 +1,36 @@
 import { readFileSync } from 'fs';
 import { parse } from 'toml';
+import Ajv from 'ajv';
 
 function read_config() {
     try {
-        const configText = readFileSync('config/config.toml', 'utf-8');
-        const config = parse(configText).crashbot || {};
+        const config = validate_config(read_toml('config/config.toml'), read_toml('config/config.schema.toml'));
         return {
-            rss_url: config?.sign_rss_feed,
-            channel_id: config?.channel_id,
+            rss_url: config.sign_rss_feed,
+            channel_id: config.channel_id,
             check_interval_ms: config?.check_interval_minutes * 60 * 1000,
         };
     } catch (err) {
         console.error('Error reading config.toml:', err);
         return { rss_url: '', channel_id: '' };
     }
+}
+
+function validate_config(data, schema) {
+    const ajv = new Ajv();
+    const validate = ajv.compile(schema);
+    const valid = validate(data);
+    if(!valid) {
+        throw new Error('Invalid configuration: ' + JSON.stringify(validate.errors));
+    }
+
+    return data.crashbot;
+}
+
+function read_toml(path) {
+    const configText = readFileSync(path, 'utf-8');
+    const config = parse(configText);
+    return config;
 }
 
 export default { read_config };
