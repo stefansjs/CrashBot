7898ee232b46c37226ad1db05199d46e0afaa700
Stefan Sullivan <stefan.sullivan@gmail.com>
2025-06-29 00:23:58 -0700

    update the bot to post different messages based on the time the last message was sent

--- js/index.mjs
+++ js/index.mjs
@@ -19,29 +19,62 @@ async function checkFeedAndPost() {
 
     const rssUrl = config['rss_url'];
     try {
+        console.debug(`reading from ${rssUrl}`);
         const feed = await parser.parseURL(rssUrl);
-        if(!feed) {
+
+        if(!feed?.items) {
             console.debug("Nothing returned from RSS feed at " + rssUrl);
             return;
         }
-
-        console.debug(`reading from ${feed.title}`);
+        
         const latest = feed.items[feed.items.length - 1];
-
-        if(!latest) {
-            console.debug(`No recent message from feed ${rssUrl}`);
-            return;
-        }
+        
         if (lastItem?.isoDate === latest?.isoDate){
-            console.debug("No new messages since " + JSON.stringify(latest));
+            console.debug("No new messages since " + latest.isoDate);
             return;
         }
         
-        await postMessage(config.channel_id, `New RSS Item: **${latest.title}**\n${latest.link}`);
-        lastItem = latest;// only assign latest to lastItem if we successfully sent the message
+        const message = buildDiscordMessage(lastItem, latest);
+        if(message) {
+            await postMessage(config.channel_id, message);
+            lastItem = latest;// only assign latest to lastItem if we successfully sent the message
+        } else {
+            console.debug("No message. Not posting to discord");
+        }
     } catch (err) {
         console.error('Error fetching RSS feed:', err);
     }
+}
+
+function buildDiscordMessage(previousMessage, currentMessage) {
+    const closedTime = getClosingTime(currentMessage);
+    const isClosed = Date.now() >= closedTime;
+
+    if (isClosed) {
+        // If the last "opened" message came long enough ago, say that we're currently closed
+        return `The space closed at ${closedTime.toLocaleString("en-US", {timeZoneName: "short"})}`;
+    } else if(getClosingTime(previousMessage) >= Date(currentMessage.isoDate)) {
+        // If the current message was sent before the previous message's close, don't re-post about how the space is open
+        return null;
+    } else {
+        // otherwise, let the caller decide if we should post this "open" message
+        return `**${latest.title}**\nSomeone opened the space at ${currentMessage.pubDate}. The space will be open for at least 1 hour. Check ${currentMessage.link} for the current status`;
+    }
+}
+
+function getClosingTime(rssMessage) {
+    const messageTime = Date.parse(rssMessage.isoDate);
+    
+    var hoursOffset = 1;
+    if(rssMessage.contentSnippet === "crashbutton1 - update - 60mins.") {
+        hoursOffset = 1;
+    }
+
+    const closedTime = new Date(messageTime + hourseToMillis(hoursOffset));
+    return closedTime;
+}
+function hourseToMillis(hours) {
+    return hours * 60 * 60 * 1000;
 }
 
 
@@ -56,6 +89,8 @@ async function setup() {
     
     //set periodic callback
     setInterval(checkFeedAndPost, config.check_interval_ms);
+    // call the callback immediately. apparently setInterval waits first
+    checkFeedAndPost();
 }
 
 await setup();
